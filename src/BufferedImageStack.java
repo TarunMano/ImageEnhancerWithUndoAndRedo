2bc496c22d8e33a45dc56ebcf97316d76ff29fb8
TarunMano <111902945+TarunMano@users.noreply.github.com>
2022-11-14 12:32:28 -0600

    BufferedImageStack2

--- src/BufferedImageStack.java
+++ src/BufferedImageStack.java
@@ -1,4 +1,30 @@
+import java.awt.image.BufferedImage;
 
 public class BufferedImageStack {
-
+	
+	public BufferedImageStack() {
+		
+	}
+	public void push(BufferedImage buffer) {
+		
+	}
+	public void pop() {
+		
+	}
+	public boolean isEmpty() {
+		return false;
+		
+	}
+	public BufferedImage get(int index) {
+		return null;
+		
+	}
+	public int getSize() {
+		return 0;
+		
+	}
+	public int getArraySize() {
+		return 0;
+		
+	}
 }
